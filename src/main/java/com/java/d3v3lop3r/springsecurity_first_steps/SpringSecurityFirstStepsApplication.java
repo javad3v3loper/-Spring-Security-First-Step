b286cb2288080e70496c6848ce0f1991d3b9380c
java.devel0p3r <java.devel0p3r@gmail.com>
2022-10-14 16:26:39 +0300

    init

---
+++ src/main/java/com/java/d3v3lop3r/springsecurity_first_steps/SpringSecurityFirstStepsApplication.java
@@ -0,0 +1,13 @@
+package com.java.d3v3lop3r.springsecurity_first_steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFirstStepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityFirstStepsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/d3v3lop3r/springsecurity_first_steps/SpringSecurityFirstStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.d3v3lop3r.springsecurity_first_steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFirstStepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
